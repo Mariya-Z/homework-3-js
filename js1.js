dacb9a6498a6690ac154383ce0128cf8b972a670
Mariya-Z <35897561+Mariya-Z@users.noreply.github.com>
2018-06-18 00:27:13 +0300

    Update js1.js

--- js1.js
+++ js1.js
@@ -1,109 +1,12 @@
 
-/*splitAndMerge("My name is John","!");
-splitAndMerge("Hello World!",",")
-*/
-reverseWord(" A fun little challenge! ");
-
-toCamelCase("the-stealth-warrior");
-stringExpansion("3D2a5d2f");
-stringExpansion('3d332f2a');
-stringExpansion('abcde');
-
-function reverseWord(str) {
-
-  let pos = 0;
-  let target = ' ';
-  let result = '';
-
-  while (true) {
-    var foundPos = str.indexOf(target, pos);
-    if (foundPos == -1) break;
-    if (foundPos == 0)
-    {
-      pos = 1;
-      result = ' ';
-      continue;
-    }
-    for (let i = foundPos - 1; i >= pos; --i){
-      result +=  str[i] ;
-    }
-    result += ' ';
-    pos = foundPos + 1;
-  }
-  // alert(result);
-  return result;
-}
-
-function stringExpansion(str){
-  let result = '';
-  let rep = 1;
-  let symbol = '';
-
-  for (i = 0; i < str.length; ++i){
-    if (isFinite(str[i])) {
-      rep = str[i];
-    }
-    else {
-      rep = +rep;
-      symbol
-
-      for (j = 0; j < rep; ++j){
-        result += str[i];
-      }
-      rep = 1;
-    }
-  }
-  // alert(result);
-  return result;
-}
-
-function toCamelCase(str){
-  let result;
-  let word;
-
-  if (str.indexOf('-'))
-  {
-    let wordsArr = str.split('-');
-    for (i = 0; i < wordsArr.length; ++i)
-    {
-      if (i == 0) {
-        result = wordsArr[0];
-      }
-
-      else {
-          word = wordsArr[i];
-          result += word[0].toUpperCase() + word.substr(1);
-      }
-    }
-  }
-  else if (str.indexOf('_'))
-  {
-    let wordsArr = str.split('_');
-    for (i = 0; i < wordsArr.length; ++i)
-    {
-      if (i == 0) {
-        result = wordsArr[0];
-      }
-
-      else {
-          word = wordsArr[i];
-          result += word[0].toUpperCase() + word.substr(1);
-      }
-    }
-  }
-  // alert(result);
-  return result;
-}
-
-
 function splitAndMerge(str, sp) {
-
   let pos = 0;
   let target = ' ';
   let result = '';
+  let foundPos;
 
   while (true) {
-    var foundPos = str.indexOf(target, pos);
+    foundPos = str.indexOf(target, pos);
     if (foundPos == -1) break;
     for (let i = pos; i < foundPos - 1; ++i){
       result +=  str[i] + sp;
@@ -117,8 +20,95 @@ function splitAndMerge(str, sp) {
     result += str[i] + sp;
   }
   result += str[str.length - 1] + ' ';
-  // alert(result);
   return result;
 }
 
+splitAndMerge("My name is John","!");
+splitAndMerge("Hello World!",",");
 
+function convert(obj){
+  let arr = [];
+  for (let key in obj){
+    let arrItem = [];
+    arrItem.push(key, obj[key]);
+    arr.push(arrItem);
+  }
+  return arr;
+}
+
+let testObj = {
+   name: 'Jeremy',
+   age: 24,
+   role: 'Software Engineer'
+};
+
+convert( testObj);
+
+function toCamelCase(str){
+  let result;
+  let word;
+  let wordsArr = str.replace(/(-|_)/g,' ').split(' ');
+
+  for (i = 0; i < wordsArr.length; ++i)
+  {
+    if (i == 0) {
+      result = wordsArr[0];
+    }
+    else {
+        word = wordsArr[i];
+        result += word[0].toUpperCase() + word.substr(1);
+    }
+  }
+  return result;
+}
+
+toCamelCase("the-stealth-warrior");
+toCamelCase("The_Stealth_Warrior");
+
+function reverseWord(str) {
+  let word = str.split(' ');
+  let result = '';
+
+  function reverse(str){
+    let res = '';
+
+    for (j = 0; j < str.length; ++j){
+      res += str[str.length - j - 1];
+    }
+    return res;
+  }
+
+  for (let i = 0; i < word.length; ++i){
+    if (word[i] != ''){
+      result += reverse(word[i]) ;
+    }
+    result += ' ';
+  }
+
+  return result.slice(0, result.length - 1);
+}
+
+reverseWord(" A fun little challenge! ");
+
+
+function stringExpansion(str){
+  let result = '';
+  let rep = 1;
+
+  for (i = 0; i < str.length; ++i){
+    if (isFinite(str[i])) {
+      rep = str[i];
+    }
+    else {
+      for (j = 0; j < rep; ++j){
+        result += str[i];
+      }
+      rep = 1;
+    }
+  }
+  return result;
+}
+
+stringExpansion("3D2a5d2f");
+stringExpansion('3d332f2a');
+stringExpansion('abcde');
